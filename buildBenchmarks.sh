62655f8aae40cfe510596c2ff8a1030be1f5cce6
Galois User <galoisuser@besspin-fpga-3.galois.com>
2020-03-29 23:44:00 -0700

    Fixing current directory

--- buildBenchmarks.sh
+++ buildBenchmarks.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+echo $(dirname $0)
+
 for PROG in adpcm_decode adpcm_encode aes basicmath blowfish crc fft limits qsort randmath rc4
 do
-	echo "Building mibench2 benchmark $PROG"
-	cd $PROG
+	echo "Building mibench2 benchmark $(dirname $0)/$PROG"
+	cd $(dirname $0)/$PROG
 	make GFE_TARGET=$1 clean
 	make GFE_TARGET=$1
 	cp main.elf ../$PROG.elf
 	make GFE_TARGET=$1 clean
-	cd ..
 	echo '`-> Done'
 done
 
